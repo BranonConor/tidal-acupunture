b574f5b04ac01096503fc2f8bf725e843af6e42b
Branon Eusebio <branoneusebio@gmail.com>
2023-11-20 17:35:02 -0800

    Added josefin sans font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import type { Metadata } from "next";
 import { Providers } from "./providers";
 import { ColorModeScript, theme } from "@chakra-ui/react";
+import { Josefin_Sans } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "TIDAL Acupunture",
   description:
     "A boutique, human-centered acupuncture clinic founded in San Diego, CA",
 };
+
+const josefinSans = Josefin_Sans({
+  weight: ["300", "400", "600"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -18,7 +24,7 @@ export default function RootLayout({
       lang="en"
       style={{ padding: "none", margin: "none", overflowX: "hidden" }}
     >
-      <body>
+      <body className={josefinSans.className}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Providers>{children}</Providers>
       </body>
