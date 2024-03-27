ae1c893469ad6755ce7f92939d1f16b3fbe57ad2
Branon Eusebio <branoneusebio@gmail.com>
2024-03-27 09:24:46 -0700

    Added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Providers } from "./providers";
 import { ColorModeScript, theme } from "@chakra-ui/react";
+import Script from "next/script";
+import * as gtag from "../gtag.js";
 
 export const metadata: Metadata = {
   title: "TIDAL Acupunture",
@@ -20,6 +22,24 @@ export default function RootLayout({
       style={{ padding: "none", margin: "none", overflowX: "hidden" }}
     >
       <body>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${gtag.GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                      });
+                    `,
+          }}
+        />
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Providers>{children}</Providers>
       </body>
