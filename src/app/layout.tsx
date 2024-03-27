f1e58ff5abea812f961660cdce30dfacdb0a7b52
Branon Eusebio <branoneusebio@gmail.com>
2024-03-27 09:44:52 -0700

    Fixed module import issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Providers } from "./providers";
 import { ColorModeScript, theme } from "@chakra-ui/react";
 import Script from "next/script";
-import * as gtag from "../gtag.js";
+import { GA_TRACKING_ID } from "../gtag";
 
 export const metadata: Metadata = {
   openGraph: {
@@ -40,7 +40,7 @@ export default function RootLayout({
       <body>
         <Script
           strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
         />
         <Script
           id="gtag-init"
@@ -50,7 +50,7 @@ export default function RootLayout({
                       window.dataLayer = window.dataLayer || [];
                       function gtag(){dataLayer.push(arguments);}
                       gtag('js', new Date());
-                      gtag('config', '${gtag.GA_TRACKING_ID}', {
+                      gtag('config', '${GA_TRACKING_ID}', {
                       page_path: window.location.pathname,
                       });
                     `,
