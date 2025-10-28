60f3f75b97e70017493d2de85fc9fc9f7f8bc00a
Branon Eusebio <branoneusebio@gmail.com>
2025-10-28 14:09:39 -0700

    Added move message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,28 @@ import { OurTeam } from "@/components/sections/OurTeam";
 import { Footer } from "@/components/Footer";
 import { Hero } from "@/components/sections/Hero";
 import { Services } from "@/components/sections/services/Services";
-import { Flex, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  Button,
+  Box,
+  useColorModeValue,
+  Text,
+  Heading,
+  Divider,
+} from "@chakra-ui/react";
 import { Info } from "@/components/sections/Info";
 import { PageLayout } from "@/components/PageLayout";
 import Link from "next/link";
 import { ContactUs } from "@/components/sections/ContactUs";
+import { UpdateBanner } from "@/components/UpdateBanner";
 
 export default function Home() {
+  const bannerBg = useColorModeValue("brand.gold", "brand.mint");
+  const bannerContainerBg = useColorModeValue("brand.wave", "brand.mauve");
+  const wavingHand = useColorModeValue("👋🏽", "👋🏻");
+
   return (
-    <PageLayout>
+    <PageLayout scrollBehavior="smooth">
       <Navbar />
       <Hero
         title="Acupuncture for all life's highs and lows."
@@ -31,6 +44,41 @@ export default function Home() {
           </Flex>
         }
       />
+      <UpdateBanner
+        width="100%"
+        bg={bannerContainerBg}
+        px={[4, 4, 8]}
+        position="relative"
+      >
+        <Box id="fall-2025-update" position="absolute" top="-64px" />
+        <Box
+          bg={bannerBg}
+          px={4}
+          py={4}
+          borderRadius={16}
+          color="brand.royalMoss"
+        >
+          <Heading variant="h2" as="h2">
+            {wavingHand} Fall 2025 Update!
+          </Heading>
+          <Divider borderColor="brand.text" mb={4} />
+          <Flex gap={4} flexDirection="column">
+            <Text>"Dear Patients,</Text>
+            <Text>
+              Tidal Acupuncture in San Diego is officially closing as we
+              transition to a new location in Santa Barbara. At this time, we
+              are no longer accepting new patients at our San Diego clinic. We
+              are so grateful for the opportunity to have supported your health
+              and wellness over the past two years and we thank you for being a
+              part of our journey.
+            </Text>
+            <Text>
+              Stay tuned for future comms on our Santa Barbara reopening! 🏝️☀️"
+            </Text>
+            <Text>- Dr. Rachel Eusebio, Founder</Text>
+          </Flex>
+        </Box>
+      </UpdateBanner>
       <WhoWeAre />
       <OurTeam />
       <Services title="Our Services" />
--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,12 +1,24 @@
 import { ReactNode } from "react";
-import { Flex, useColorModeValue } from "@chakra-ui/react";
+import {
+  ComponentDefaultProps,
+  Flex,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { CloseIcon } from "@chakra-ui/icons";
+import Link from "next/link";
 
-interface PageLayoutProps {
+interface PageLayoutProps extends ComponentDefaultProps {
   children?: ReactNode;
 }
 
 export const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
   const bg = useColorModeValue("brand.sand", "brand.royalMoss");
+  const updateButtonBg = useColorModeValue(
+    "rgba(255, 237, 202, 0.25)",
+    "rgba(8, 40, 48, 0.20)"
+  );
+  const wavingHand = useColorModeValue("👋🏽", "👋🏻");
 
   return (
     <Flex
@@ -21,6 +33,43 @@ export const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
       boxSizing="border-box"
     >
       {children}
+      <Flex
+        as={Link}
+        href="/#fall-2025-update"
+        position="fixed"
+        bottom={4}
+        right={4}
+        zIndex={99}
+        bg={updateButtonBg}
+        alignItems="center"
+        justifyContent="center"
+        borderRadius="100%"
+        width={12}
+        height={12}
+        backdropFilter="blur(10px)"
+        boxShadow="md"
+      >
+        <motion.div
+          animate={{
+            rotate: [0, 0, 45, -15, 50, 0, 45, 0, 0],
+          }}
+          transition={{
+            duration: 2,
+            ease: "easeInOut",
+            times: [0, 0.2, 0.5, 0.8, 1, 2],
+            repeat: Infinity,
+            repeatDelay: 2,
+          }}
+          style={{
+            position: "relative",
+            left: "-1px",
+            top: "1px",
+            scale: "1.25",
+          }}
+        >
+          {wavingHand}
+        </motion.div>
+      </Flex>
     </Flex>
   );
 };
